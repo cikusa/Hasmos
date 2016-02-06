7c976bf773efb323172d03152143d76497e8961f
cikusa <tylerblugandersen@gmail.com>
2016-02-06 14:37:15 +0800

    commit

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
@@ -12,13 +13,17 @@ import System.Exit
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 
-import Graphics.Gloss
+import Graphics.Gloss.Game hiding (Event)
 import Graphics.Gloss.Interface.IO.Game hiding (Event)
 import qualified Graphics.Gloss.Interface.IO.Game as G
 
 import Control.Monad.Freer
 import Control.Monad.Freer.Internal
 import Control.Monad.Freer.Reader
+
+import Control.Arrow
+
+import Data.Maybe
 
 import Data.IORef
 import Debug.Trace
@@ -62,33 +67,74 @@ runLift (E u q) =
 instance (Member (Lift Moment) r) => MonadMoment (Eff r) where
   liftMoment = lift
 
-type DtMoment r = (Member (Lift Moment) r, Member (Reader (Event Float)) r)
-
 runLiftMoment :: MonadMoment m => Eff '[Lift Moment] w -> m w
 runLiftMoment = liftMoment . runLift
 
-timeAccumulator :: DtMoment r => Eff r (Behavior Float)
-timeAccumulator = do
-  dt <- ask
-  accumB 0 $ (+) <$> dt
+type GameMoment r =
+  ( Member (Lift Moment) r
+  , Member (Reader GameState) r )
 
-zoomingCircle :: DtMoment r => Float -> Color -> Eff r (Behavior Picture)
-zoomingCircle r rawColor =
-  fmap updateCircle <$> timeAccumulator
+data GameState = GameState
+  { deltaTime  :: Event Float
+  , windowSize :: Size }
+
+getTick :: GameMoment r => Eff r (Event Float)
+getTick = deltaTime <$> ask
+
+createTimer :: GameMoment r => Eff r (Behavior Float)
+createTimer = do
+  eTick  <- getTick
+  accumB 0 $ (+) <$> eTick
+
+type Duration      = Float
+type TimedObject a = (Duration, a)
+
+timeline :: GameMoment r => Event [TimedObject a] -> (Float -> a -> Maybe a) -> Eff r (Behavior [TimedObject a])
+timeline eobjs transform = do
+  eTick <- getTick
+  queue <-
+    accumB ([], []) $ unionWith const
+      (update  <$> eTick)
+      (enqueue <$> eobjs)
+  return $ fst <$> queue
+  where
+    update dt (acts, []) = (updateActs dt acts, [])
+    update dt (acts, (d, v):xs)
+      | d < 0     = (updateActs dt ((0, v):acts), xs)
+      | otherwise = (updateActs dt acts, (d - dt, v):xs)
+    updateActs dt = catMaybes . fmap updateAct
+      where
+        updateAct (ot, v) =
+          let nt = ot + dt in (nt,) <$> transform nt v
+    enqueue newobjs = second (++newobjs)
+
+loadingScreen :: GameMoment r => Eff r (Behavior Picture)
+loadingScreen = do
+  eTick  <- getTick
+  bTimer <- accumB 0 $ (+) <$> eTick
+  return $ updateCircle <$> bTimer
   where
     updateCircle t =
       let prog = abs (sin $ t * pi / 2)
-          cirR = r * prog
+          cirR = fullRadius * prog
           cirC = withAlpha prog rawColor in
       color cirC $ circleSolid cirR
+    rawColor = makeColorI 191 202 230 255
+    fullRadius = 200
 
-mMain :: Event Float -> Event InputEvent -> MomentIO (Behavior Picture)
-mMain dt input = do
+mMain :: Size -> Event Float -> Event InputEvent -> MomentIO (Behavior Picture)
+mMain winSize dt input = do
   reactimate $ const exitSuccess <$> filterE isExitMsg input
-  runLiftMoment $ runReader (zoomingCircle 200 $ makeColorI 191 202 230 255) dt
+  runLiftMoment $ runReader loadingScreen
+    GameState
+      { deltaTime  = dt
+      , windowSize = winSize }
   where
     isExitMsg (EventKey (SpecialKey KeyEsc) Down _ _) = True
     isExitMsg _ = False
 
 main :: IO ()
-main = playBanana (InWindow "Hasmos" (1024, 768) (0, 0)) white 60 mMain
+main = playBanana (InWindow "Hasmos" winSize (0, 0)) white 60 $ mMain winSizeF
+  where
+    winSize@(w, h) = (1024, 768)
+    winSizeF = (fromIntegral w, fromIntegral h)
